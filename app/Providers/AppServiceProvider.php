2a4faa1685d6028e5b786d212b7633af27a74516
Taouil-abdo <abdotaouil03@gmail.com>
2025-04-23 13:30:43 +0100

    Refactor AppServiceProvider to improve service registration for repositories

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,16 +2,27 @@
 
 namespace App\Providers;
 
+use App\Repositories\RoomRepository;
+use App\Repositories\UserRepository;
 use Illuminate\Support\ServiceProvider;
+use App\Repositories\CategoryRepository;
+use App\Repositories\RoomRepositoryInterface;
+use App\Repositories\UserRepositoryInterface;
+use App\Repositories\CategoryRepositoryInterface;
+// use Illuminate\Support\Facades\File;
+
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    public function register(): void
+    public function register()
     {
-        //
+        $this->app->bind(RoomRepositoryInterface::class, RoomRepository::class);
+        $this->app->bind(CategoryRepositoryInterface::class, CategoryRepository::class);
+        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+
     }
 
     /**
