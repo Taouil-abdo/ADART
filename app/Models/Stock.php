f6cb8bb9be5f4a254960bb8bc58fd966d3e5db67
Taouil-abdo <abdotaouil03@gmail.com>
2025-04-23 14:35:32 +0100

    Add Stock model relationships and fillable properties for enhanced data management

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -9,4 +9,16 @@ class Stock extends Model
 {
     /** @use HasFactory<\Database\Factories\StockFactory> */
     use HasFactory;
+    protected $fillable = ['stock_id', 'item_name', 'quantity', 'stock_status', 'unit','category_id', 'supplier_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function supplier(){
+        return $this->belongsTo(supplier::class);
+    }
+   
+
 }
